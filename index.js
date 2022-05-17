31f9f7ba259dc4d719c8936f3ece941f931bdd98
hwangsungmin <hwangsungmin_00@naver.com>
2022-05-17 14:18:46 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/Users");
+
+//각 타입을 분석해서 가져오게 해줌
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose=require('mongoose')
-mongoose.connect('mongodb+srv://sungmin:sungmin1219@boilerplate.0c1re.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     //useNewUrlParser: true, useUnifiedTepology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=>console.log('MongoDB Connected...'))
   .catch(err=>console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 새해복 많이 받으세요~~~')
 })
+
+
+
+app.post('/register', (req, res)=>{
+    //회원가입 할때 필요한 정보들을 클라이언트에서 가져오면
+    //그것들을 데베에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo)=>{
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
